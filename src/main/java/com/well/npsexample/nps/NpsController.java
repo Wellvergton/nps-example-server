f26c1de365f7799c6cf15c0c9923fe462a4e1e1f
Wellvergton <wmc.jp@outlook.com>
2021-06-04 20:18:51 -0300

    Restricted CORS

--- src/main/java/com/well/npsexample/nps/NpsController.java
+++ src/main/java/com/well/npsexample/nps/NpsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://nps-example.web.app/")
 @RestController
 @RequestMapping("/nps")
 public class NpsController {
